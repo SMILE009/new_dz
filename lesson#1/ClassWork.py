fd59575c8e4c3966b5b7a1bd5baf45468e3f6b90
SMILE <a2katvic@gmail.com>
2017-02-02 01:11:20 +0200

    smt

--- lesson#1/ClassWork.py
+++ lesson#1/ClassWork.py
@@ -2,6 +2,7 @@ def myFunc():
     a = 5
     b = 4
     x = a + b
-    print('I am a function, this is your result a + b - ' + x)
+    print('I am a function, this is your result a + b - '.join(x))
 
 myFunc()
+
